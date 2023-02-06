461c05f3ea1bdacaffad04a376652b12d1b1480e
2227500 <115985919+2227500@users.noreply.github.com>
2023-02-06 11:00:57 +0000

    Added enemy vehicles
    Scripted enemy vehicles moving in the opposite direction as the player

--- Prototype 1/Assets/Scripts/AiVechicleMovement.cs
+++ Prototype 1/Assets/Scripts/AiVechicleMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AiVechicleMovement : MonoBehaviour
 {
+    public float speed = 15.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class AiVechicleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
